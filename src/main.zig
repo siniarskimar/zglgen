273b3446b613b16834b067d47dbc57b687aeb04a
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2024-01-03 14:33:45 +0100

    Refactor a function out of parseXmlTag

--- src/main.zig
+++ src/main.zig
@@ -96,7 +96,7 @@ const XmlTag = union(enum) {
     };
 
     const XmlDecl = struct {
-        version: std.SemanticVersion,
+        version: std.SemanticVersion = std.SemanticVersion{ .major = 1, .minor = 0, .patch = 0 },
     };
 };
 
@@ -144,19 +144,80 @@ fn skipWhitespace(reader: anytype) !u8 {
     };
 }
 
+fn parseXmlTagAttributes(comptime TagType: type, tag: *TagType, allocator: std.mem.Allocator, stream: anytype, reader: anytype) !void {
+    switch (TagType) {
+        XmlTag.StartTag, XmlTag.XmlDecl => {},
+        else => @compileError("Unsupported tag type"),
+    }
+    const buffer = stream.buffer;
+
+    while (true) {
+        const ch = skipWhitespace(reader) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+        if (TagType == XmlTag.StartTag and ch == '/') {
+            if (stream.pos == buffer.len) {
+                tag.self_close = true;
+                break;
+            }
+            return error.InvalidChar;
+        }
+        if (TagType == XmlTag.XmlDecl and ch == '?') {
+            break;
+        }
+        try stream.seekBy(-1);
+
+        const attrname_start = stream.pos;
+        const attrname_len = readXmlName(reader, null) catch |err| switch (err) {
+            error.InvalidChar => if (buffer[stream.pos - 1] == '=')
+                stream.pos - attrname_start
+            else
+                return err,
+
+            else => return err,
+        };
+
+        const maybe_quote = try skipWhitespace(reader);
+        const quote = if (maybe_quote == '=') try skipWhitespace(reader) else maybe_quote;
+
+        if (quote != '\'' and quote != '"') {
+            return error.InvalidChar;
+        }
+        const attrvalue_start = stream.pos;
+        try reader.skipUntilDelimiterOrEof(quote);
+        const attrvalue_len = stream.pos - attrvalue_start;
+
+        const attrname = buffer[attrname_start .. attrname_start + attrname_len];
+        const attrvalue = buffer[attrvalue_start .. attrvalue_start + attrvalue_len];
+        switch (TagType) {
+            XmlTag.StartTag => try tag.attributes.put(allocator, attrname, attrvalue),
+            XmlTag.XmlDecl => {
+                if (std.mem.eql(u8, attrname, "version")) {
+                    var it = std.mem.splitScalar(u8, attrvalue, '.');
+                    const major = try std.fmt.parseInt(u32, it.next() orelse "0", 10);
+                    const minor = try std.fmt.parseInt(u32, it.next() orelse "0", 10);
+                    tag.version = std.SemanticVersion{ .major = major, .minor = minor, .patch = 0 };
+                }
+            },
+            else => @compileError("Unsupported tag type"),
+        }
+    }
+}
+
 fn parseXmlTag(allocator: std.mem.Allocator, buffer: []const u8) !XmlTag {
     var stream = std.io.fixedBufferStream(buffer);
     const reader = stream.reader();
 
     switch (try reader.readByte()) {
         '?' => {
+            var tag = XmlTag.XmlDecl{};
             const name = try reader.readBytesNoEof(3);
             if (!std.mem.eql(u8, &name, "xml")) {
                 return error.UnknownTag;
             }
-            return .{ .xml_decl = .{
-                .version = std.SemanticVersion{ .major = 1, .minor = 0, .patch = 0 },
-            } };
+            try parseXmlTagAttributes(XmlTag.XmlDecl, &tag, allocator, &stream, reader);
+            return .{ .xml_decl = tag };
         },
         '/' => {
             const name_len = readXmlName(reader, null) catch |err| switch (err) {
@@ -213,44 +274,7 @@ fn parseXmlTag(allocator: std.mem.Allocator, buffer: []const u8) !XmlTag {
                 return .{ .start_tag = tag };
             }
 
-            while (true) {
-                const ch = skipWhitespace(reader) catch |err| switch (err) {
-                    error.EndOfStream => break,
-                    else => return err,
-                };
-                if (ch == '/') {
-                    if (stream.pos == buffer.len) {
-                        tag.self_close = true;
-                        break;
-                    }
-                    return error.InvalidChar;
-                }
-                try stream.seekBy(-1);
-
-                const attrname_start = stream.pos;
-                const attrname_len = readXmlName(reader, null) catch |err| switch (err) {
-                    error.InvalidChar => if (buffer[stream.pos - 1] == '=')
-                        stream.pos - attrname_start
-                    else
-                        return err,
-                    else => return err,
-                };
-
-                const maybe_quote = try skipWhitespace(reader);
-                const quote = if (maybe_quote == '=') try skipWhitespace(reader) else maybe_quote;
-
-                if (quote != '\'' and quote != '"') {
-                    return error.InvalidChar;
-                }
-                const attrvalue_start = stream.pos;
-                try reader.skipUntilDelimiterOrEof(quote);
-                const attrvalue_len = stream.pos - attrvalue_start;
-
-                const attrname = buffer[attrname_start .. attrname_start + attrname_len];
-                const attrvalue = buffer[attrvalue_start .. attrvalue_start + attrvalue_len];
-
-                try tag.attributes.put(allocator, attrname, attrvalue);
-            }
+            try parseXmlTagAttributes(XmlTag.StartTag, &tag, allocator, &stream, reader);
 
             return .{ .start_tag = tag };
         },
