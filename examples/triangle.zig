71be1cca1079b92092c8a7c5b101257e87bbd8fb
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2024-02-27 12:22:40 +0100

    Drop 'gl' prefix in examples

--- examples/triangle.zig
+++ examples/triangle.zig
@@ -73,85 +73,85 @@ pub fn main() !void {
 
     glfw.glfwSwapInterval(1);
     {
-        const version_str = gl.glGetString(gl.GL_VERSION) orelse "<no version string>";
+        const version_str = gl.getString(gl.GL_VERSION) orelse "<no version string>";
         std.log.info("OpenGL {s}\n", .{version_str});
     }
 
-    gl.glEnable(gl.GL_DEBUG_OUTPUT);
-    gl.glDebugMessageCallback(glMessageCallback, null);
-    gl.glDebugMessageControl(gl.GL_DONT_CARE, gl.GL_DONT_CARE, gl.GL_DONT_CARE, 0, null, gl.GL_TRUE);
+    gl.enable(gl.GL_DEBUG_OUTPUT);
+    gl.debugMessageCallback(glMessageCallback, null);
+    gl.debugMessageControl(gl.GL_DONT_CARE, gl.GL_DONT_CARE, gl.GL_DONT_CARE, 0, null, gl.GL_TRUE);
 
-    var program: gl.GLuint = gl.glCreateProgram();
+    var program: gl.GLuint = gl.createProgram();
     {
-        var vs: gl.GLuint = gl.glCreateShader(gl.GL_VERTEX_SHADER);
-        defer gl.glDeleteShader(vs);
+        var vs: gl.GLuint = gl.createShader(gl.GL_VERTEX_SHADER);
+        defer gl.deleteShader(vs);
 
-        var fs: gl.GLuint = gl.glCreateShader(gl.GL_FRAGMENT_SHADER);
-        defer gl.glDeleteShader(fs);
+        var fs: gl.GLuint = gl.createShader(gl.GL_FRAGMENT_SHADER);
+        defer gl.deleteShader(fs);
 
-        gl.glShaderSource(
+        gl.shaderSource(
             vs,
             1,
             &[_][*c]const u8{VS_SOURCE},
             &[_:0]gl.GLint{ VS_SOURCE_LEN, 0 },
         );
-        gl.glShaderSource(
+        gl.shaderSource(
             fs,
             1,
             &[_][*c]const u8{FS_SOURCE},
             &[_:0]gl.GLint{ FS_SOURCE_LEN, 0 },
         );
-        gl.glCompileShader(vs);
-        gl.glCompileShader(fs);
+        gl.compileShader(vs);
+        gl.compileShader(fs);
 
         var status: gl.GLuint = 0;
-        gl.glGetShaderiv(vs, gl.GL_COMPILE_STATUS, @ptrCast(&status));
+        gl.getShaderiv(vs, gl.GL_COMPILE_STATUS, @ptrCast(&status));
         if (status == 0) {
             var infolog_len: gl.GLint = 0;
-            gl.glGetShaderiv(vs, gl.GL_INFO_LOG_LENGTH, @ptrCast(&infolog_len));
+            gl.getShaderiv(vs, gl.GL_INFO_LOG_LENGTH, @ptrCast(&infolog_len));
 
             var buffer = try allocator.alloc(u8, @intCast(infolog_len));
             defer allocator.free(buffer);
 
-            gl.glGetShaderInfoLog(vs, infolog_len, null, buffer.ptr);
+            gl.getShaderInfoLog(vs, infolog_len, null, buffer.ptr);
             std.log.err("[GL] Vertex shader compilation failed:\n{s}", .{buffer});
             return error.ShaderCompilationFailed;
         }
 
-        gl.glGetShaderiv(fs, gl.GL_COMPILE_STATUS, @ptrCast(&status));
+        gl.getShaderiv(fs, gl.GL_COMPILE_STATUS, @ptrCast(&status));
         if (status == 0) {
             var infolog_len: gl.GLint = 0;
-            gl.glGetShaderiv(fs, gl.GL_INFO_LOG_LENGTH, @ptrCast(&infolog_len));
+            gl.getShaderiv(fs, gl.GL_INFO_LOG_LENGTH, @ptrCast(&infolog_len));
 
             var buffer = try allocator.alloc(u8, @intCast(infolog_len));
             defer allocator.free(buffer);
 
-            gl.glGetShaderInfoLog(fs, infolog_len, null, buffer.ptr);
+            gl.getShaderInfoLog(fs, infolog_len, null, buffer.ptr);
             std.log.err("[GL] Fragment shader compilation failed:\n{s}", .{buffer});
             return error.ShaderCompilationFailed;
         }
 
-        gl.glAttachShader(program, vs);
-        gl.glAttachShader(program, fs);
+        gl.attachShader(program, vs);
+        gl.attachShader(program, fs);
 
-        gl.glBindAttribLocation(program, 0, "Pos");
-        gl.glBindAttribLocation(program, 1, "Color");
-        gl.glLinkProgram(program);
+        gl.bindAttribLocation(program, 0, "Pos");
+        gl.bindAttribLocation(program, 1, "Color");
+        gl.linkProgram(program);
 
-        gl.glGetProgramiv(program, gl.GL_LINK_STATUS, @ptrCast(&status));
+        gl.getProgramiv(program, gl.GL_LINK_STATUS, @ptrCast(&status));
         if (status == 0) {
             var infolog_len: gl.GLint = 0;
-            gl.glGetProgramiv(program, gl.GL_INFO_LOG_LENGTH, @ptrCast(&infolog_len));
+            gl.getProgramiv(program, gl.GL_INFO_LOG_LENGTH, @ptrCast(&infolog_len));
 
             var buffer = try allocator.alloc(u8, @intCast(infolog_len));
             defer allocator.free(buffer);
 
-            gl.glGetProgramInfoLog(program, infolog_len, null, buffer.ptr);
+            gl.getProgramInfoLog(program, infolog_len, null, buffer.ptr);
             std.log.err("[GL] Program linking failed!\n{s}", .{buffer});
             return error.ProgramLinkingFailed;
         }
-        gl.glDetachShader(program, vs);
-        gl.glDetachShader(program, fs);
+        gl.detachShader(program, vs);
+        gl.detachShader(program, fs);
     }
 
     const verticies = [_]f32{
@@ -164,27 +164,27 @@ pub fn main() !void {
 
     var vao: gl.GLuint = 0;
     var vbo: gl.GLuint = 0;
-    gl.glGenVertexArrays(1, &vao);
-    gl.glBindVertexArray(vao);
+    gl.genVertexArrays(1, &vao);
+    gl.bindVertexArray(vao);
 
-    gl.glGenBuffers(1, &vbo);
-    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo);
-    gl.glBufferData(gl.GL_ARRAY_BUFFER, @sizeOf([verticies.len]f32), &verticies, gl.GL_STATIC_DRAW);
+    gl.genBuffers(1, &vbo);
+    gl.bindBuffer(gl.GL_ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.GL_ARRAY_BUFFER, @sizeOf([verticies.len]f32), &verticies, gl.GL_STATIC_DRAW);
 
-    gl.glEnableVertexAttribArray(0);
-    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, @sizeOf(f32) * 6, @ptrFromInt(0));
+    gl.enableVertexAttribArray(0);
+    gl.vertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, @sizeOf(f32) * 6, @ptrFromInt(0));
 
-    gl.glEnableVertexAttribArray(1);
-    gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, @sizeOf(f32) * 6, @ptrFromInt(@sizeOf(f32) * 3));
+    gl.enableVertexAttribArray(1);
+    gl.vertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, @sizeOf(f32) * 6, @ptrFromInt(@sizeOf(f32) * 3));
 
-    gl.glUseProgram(program);
-    gl.glClearColor(0.0, 0.0, 0.0, 1.0);
+    gl.useProgram(program);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     while (glfw.glfwWindowShouldClose(window) == 0) {
         glfw.glfwPollEvents();
 
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT);
-        // gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3);
-        gl.glDrawElements(gl.GL_TRIANGLES, 3, gl.GL_UNSIGNED_INT, &indecies);
+        gl.clear(gl.GL_COLOR_BUFFER_BIT);
+        // gl.drawArrays(gl.GL_TRIANGLES, 0, 3);
+        gl.drawElements(gl.GL_TRIANGLES, 3, gl.GL_UNSIGNED_INT, &indecies);
 
         glfw.glfwSwapBuffers(window);
     }
