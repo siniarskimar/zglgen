f0131c29af2f2e4e1a1698ccb107050ebfff6dc8
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2024-03-03 20:26:33 +0100

    Support loading a subset of generated functions
    Closes #13

--- src/glregistry.zig
+++ src/glregistry.zig
@@ -92,6 +92,10 @@ pub const Registry = struct {
             gles2,
             glsc2,
         };
+
+        pub fn asKey(self: @This()) FeatureKey {
+            return .{ .api = self.api, .number = self.number };
+        }
     };
 
     const Requirement = union(enum) {
@@ -181,7 +185,7 @@ pub const Registry = struct {
 
         return self.features.items[feature_start..feature_end];
     }
-    pub fn getFeature(self: @This(), feature: FeatureRef) ?Feature {
+    pub fn getFeature(self: @This(), feature: FeatureKey) ?Feature {
         for (self.features.items) |feat| {
             if (feat.api == feature.api and feat.number.order(feature) == .eq) {
                 return feat;
@@ -618,7 +622,13 @@ const ModuleRequirements = struct {
     allocator: std.mem.Allocator,
     enumgroups: std.StringHashMapUnmanaged(void) = .{},
     enums: std.ArrayListUnmanaged(Registry.Enum) = .{},
-    commands: std.ArrayListUnmanaged(Registry.Command) = .{},
+    commands: std.ArrayListUnmanaged(CommandInfo) = .{},
+
+    const CommandInfo = struct {
+        command: Registry.Command,
+        feature: ?FeatureKey = null,
+        ext: ?ExtensionKey = null,
+    };
 
     pub fn deinit(self: *@This()) void {
         self.enumgroups.deinit(self.allocator);
@@ -636,7 +646,7 @@ fn resolveRequirements(
     errdefer result.deinit();
 
     var req_it = requirement_set.valueIterator();
-    while (req_it.next()) |req| switch (req.*) {
+    while (req_it.next()) |req_ref| switch (req_ref.req) {
         .@"enum" => |ename| {
             const e: Registry.Enum = registry.enums.get(ename) orelse unreachable;
 
@@ -662,7 +672,11 @@ fn resolveRequirements(
                     try result.enumgroups.put(allocator, egroup_name, {});
                 }
             }
-            try result.commands.append(allocator, command);
+            try result.commands.append(allocator, .{
+                .command = command,
+                .feature = req_ref.feature_ref,
+                .ext = req_ref.ext_ref,
+            });
         },
         .type => {},
     };
@@ -671,13 +685,14 @@ fn resolveRequirements(
 }
 
 fn writeProcedureTable(
-    registry: *const Registry,
-    commands: std.ArrayListUnmanaged(Registry.Command),
+    // registry: *const Registry,
+    command_refs: std.ArrayListUnmanaged(ModuleRequirements.CommandInfo),
     writer: anytype,
 ) !void {
     try writer.writeAll("pub const ProcTable = struct {\n");
 
-    for (commands.items) |command| {
+    for (command_refs.items) |command_ref| {
+        const command = command_ref.command;
         try writer.print("{s}: ", .{command.name});
 
         for (command.name) |c| {
@@ -685,7 +700,9 @@ fn writeProcedureTable(
         }
         try writer.writeAll("PROC = null,\n");
     }
-    for (commands.items) |command| {
+    for (command_refs.items) |command_ref| {
+        const command = command_ref.command;
+        const params = command.params;
         try writer.writeAll("pub const ");
 
         for (command.name) |c| {
@@ -693,20 +710,20 @@ fn writeProcedureTable(
         }
         try writer.writeAll("PROC = ?*const fn(\n");
 
-        const param_len = command.params.items.len;
+        const param_len = params.items.len;
 
-        if (command.params.items.len > 0) {
-            for (command.params.items[0 .. param_len - 1]) |param| {
+        if (params.items.len > 0) {
+            for (params.items[0 .. param_len - 1]) |param| {
                 try writer.print("{s}: {s},", .{ param.name, param.type });
             }
-            const last_param = command.params.items[param_len - 1];
+            const last_param = params.items[param_len - 1];
             try writer.print("{s}: {s}", .{ last_param.name, last_param.type });
         }
         try writer.print(") callconv(.C) {s};", .{command.return_type});
     }
 
     try writer.writeAll("};\n");
-    _ = registry;
+    // _ = registry;
 }
 
 const MODULE_TYPE_PREAMPLE =
@@ -902,12 +919,22 @@ pub fn writeFunctionParameterName(param: Registry.Command.Param, index: ?usize, 
     }
 }
 
-const FeatureRef = struct {
+/// Fields uniquely identifying a feature
+const FeatureKey = struct {
     api: Registry.Feature.Api,
     number: std.SemanticVersion,
 };
 
-const RequirementSet = std.StringHashMap(Registry.Requirement);
+/// Fields uniquely identifying an extension
+const ExtensionKey = []const u8;
+
+const RequirementInfo = struct {
+    req: Registry.Requirement,
+    feature_ref: ?FeatureKey = null,
+    ext_ref: ?ExtensionKey = null,
+};
+
+const RequirementSet = std.StringHashMap(RequirementInfo);
 
 /// Obtains a set of requirements needed to generate
 /// a module up to a given feature.
@@ -915,7 +942,7 @@ const RequirementSet = std.StringHashMap(Registry.Requirement);
 fn getRequirementSet(
     allocator: std.mem.Allocator,
     registry: *Registry,
-    feature_ref: FeatureRef,
+    feature_ref: FeatureKey,
     extensions: []const []const u8,
 ) !RequirementSet {
     var requirement_set = RequirementSet.init(allocator);
@@ -933,7 +960,10 @@ fn getRequirementSet(
             if (getorput_res.found_existing) {
                 continue;
             }
-            getorput_res.value_ptr.* = req;
+            getorput_res.value_ptr.* = .{
+                .req = req,
+                .feature_ref = .{ .api = feature.api, .number = feature.number },
+            };
         }
         if (feature_ref.api == .glcore and feature.api == .gl) for (feature.remove_set.items) |req| {
             _ = requirement_set.remove(req.name());
@@ -955,7 +985,10 @@ fn getRequirementSet(
                 if (getorput_res.found_existing) {
                     continue;
                 }
-                getorput_res.value_ptr.* = req;
+                getorput_res.value_ptr.* = .{
+                    .req = req,
+                    .ext_ref = extname,
+                };
             }
         } else {
             std.log.warn("Extension '{s}' not found! Skipping!", .{extname});
@@ -1017,29 +1050,67 @@ pub fn writeFunction(command: Registry.Command, writer: anytype) !void {
     try writer.writeAll("});\n}\n");
 }
 
-pub fn writeFeatureLoaderFunction(feature_ref: FeatureRef, requirements: ModuleRequirements, writer: anytype) !void {
+pub fn writeFeatureLoaderFunction(feature: FeatureKey, requirements: ModuleRequirements, writer: anytype) !void {
     try writer.writeAll(
-        \\threadlocal var current_proc_table: ?ProcTable = null;
-        \\
         \\pub fn load
     );
 
-    for (@tagName(feature_ref.api)) |c| {
+    for (@tagName(feature.api)) |c| {
+        try writer.writeByte(std.ascii.toUpper(c));
+    }
+    try writer.print("{}{}", .{ feature.number.major, feature.number.minor });
+
+    try writer.writeAll(
+        \\(proc_table: *ProcTable, getProcAddress: GETPROCADDRESSPROC) !void {
+    );
+
+    for (requirements.commands.items) |command_info| {
+        const cmd_feature = command_info.feature orelse continue;
+        const feature_order = cmd_feature.number.order(feature.number);
+
+        if (cmd_feature.api != feature.api or feature_order != .eq) {
+            continue;
+        }
+        try writer.print(
+            "proc_table.{0s} = @ptrCast(getProcAddress(\"{0s}\") orelse return error.LoadError);\n",
+            .{command_info.command.name},
+        );
+    }
+
+    try writer.writeAll(
+        \\}
+        \\
+    );
+}
+pub fn writeExtensionLoaderFunction(ext: ExtensionKey, requirements: ModuleRequirements, writer: anytype) !void {
+    try writer.writeAll(
+        \\pub fn load
+    );
+
+    for (ext) |c| {
         try writer.writeByte(std.ascii.toUpper(c));
     }
 
     try writer.writeAll(
-        \\(getProcAddress: GETPROCADDRESSPROC) !ProcTable {
-        \\  return ProcTable {
+        \\(proc_table: *ProcTable, getProcAddress: GETPROCADDRESSPROC) !void {
     );
 
-    for (requirements.commands.items) |command| {
-        try writer.print(".{0s} = @ptrCast(getProcAddress(\"{0s}\") orelse return error.LoadError),\n", .{command.name});
+    for (requirements.commands.items) |command_info| {
+        const cmd_ext = command_info.ext orelse continue;
+
+        if (!std.mem.eql(u8, cmd_ext, ext)) {
+            continue;
+        }
+
+        try writer.print(
+            "proc_table.{0s} = @ptrCast(getProcAddress(\"{0s}\") orelse return error.LoadError);\n",
+            .{command_info.command.name},
+        );
     }
 
     try writer.writeAll(
-        \\  };
         \\}
+        \\
     );
 }
 
@@ -1070,7 +1141,7 @@ fn writeEnum(@"enum": Registry.Enum, writer: anytype) !void {
 pub fn generateModule(
     allocator: std.mem.Allocator,
     registry: *Registry,
-    feature_ref: FeatureRef,
+    feature_ref: FeatureKey,
     extensions: []const []const u8,
     writer: anytype,
 ) !void {
@@ -1094,19 +1165,77 @@ pub fn generateModule(
         try writeEnum(e, writer);
     }
 
-    try writeProcedureTable(registry, requirements.commands, writer);
+    try writeProcedureTable(requirements.commands, writer);
 
-    for (requirements.commands.items) |command| {
-        try writeFunction(command, writer);
+    for (requirements.commands.items) |command_ref| {
+        try writeFunction(command_ref.command, writer);
     }
 
-    try writeFeatureLoaderFunction(feature_ref, requirements, writer);
+    try writer.writeAll(
+        \\threadlocal var current_proc_table: ?ProcTable = null;
+        \\
+    );
+
+    const feature_range = registry.getFeatureRange(feature_ref.api);
+    for (feature_range) |feature| {
+        if (feature.number.order(feature_ref.number) == .gt) {
+            continue;
+        }
+        try writeFeatureLoaderFunction(feature.asKey(), requirements, writer);
+    }
+
+    try writer.writeAll(
+        \\pub fn load
+    );
+
+    for (@tagName(feature_ref.api)) |c| {
+        try writer.writeByte(std.ascii.toUpper(c));
+    }
+
+    try writer.writeAll(
+        \\(getProcAddress: GETPROCADDRESSPROC) !ProcTable {
+        \\    var table = ProcTable{};
+    );
+
+    for (feature_range) |feature| {
+        if (feature.number.order(feature_ref.number) == .gt) {
+            continue;
+        }
+        const tagname = @tagName(feature.api);
+
+        try writer.writeAll(
+            \\    load
+        );
+        for (tagname) |c| {
+            try writer.writeByte(std.ascii.toUpper(c));
+        }
+
+        try writer.print("{}{}(&table,getProcAddress) catch return error.", .{ feature.number.major, feature.number.minor });
+
+        for (tagname) |c| {
+            try writer.writeByte(std.ascii.toUpper(c));
+        }
+        try writer.print("{}{}LoadFailed;\n", .{ feature.number.major, feature.number.minor });
+    }
+
+    try writer.writeAll(
+        \\    return table;
+        \\}
+    );
+
+    for (extensions) |ext| {
+        _ = registry.extensions.get(ext) orelse {
+            std.log.warn("Extension {s} not found in registry! Cannot write loader function!", .{ext});
+            return error.ExtensionNotFound;
+        };
+        try writeExtensionLoaderFunction(ext, requirements, writer);
+    }
 
     try writer.writeAll(
         \\pub fn makeProcTableCurrent(proc_table: ?ProcTable) void {
         \\      current_proc_table = proc_table;
         \\}
-        \\pub fn getProcTablePtr() ?*const ProcTable {
+        \\pub fn getProcTablePtr() ?*ProcTable {
         \\      return &(current_proc_table orelse return null);
         \\}
         \\
